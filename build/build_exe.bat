881b9b289aed3dddddd1d9bb6b5267c0c60fe8b1
homa4ella <krutyehomaibusa@gmail.com>
2026-02-21 18:07:58 +0500

    Update build_exe.bat

--- build/build_exe.bat
+++ build/build_exe.bat
@@ -1,6 +1,1 @@
-@echo off
-echo Сборка HomRec.exe...
-pip install pyinstaller opencv-python
-pyinstaller --onefile --windowed --icon=../icons/homrec.ico --name=HomRec ../HomRec.py
-echo Готово!
-pause
+
